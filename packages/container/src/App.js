826f7425365ae78953eaa6288e138ca1cf16cd1a
Piotr Wawrzynkiewicz <pio.wawrzynkiewicz@gmail.com>
2023-09-01 19:30:37 +0200

    Fixed styles conflicts

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
-import Header from './Header';
+import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({ productionPrefix: 'con' });
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };

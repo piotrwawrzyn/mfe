51c9594320c2a1a8828a7a99fa9b4ef681eff9c3
Piotr Wawrzynkiewicz <pio.wawrzynkiewicz@gmail.com>
2023-09-01 18:36:53 +0200

    Added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,8 @@ const packageJson = require('../package.json');
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js' // Prevent caching issues
+    filename: '[name].[contenthash].js', // Prevent caching issues
+    publicPath: '/marketing/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({

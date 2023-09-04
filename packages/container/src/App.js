129cdc96ae38d6ba27c1afb4553ef6e76e0e2ac9
Piotr Wawrzynkiewicz <pio.wawrzynkiewicz@gmail.com>
2023-09-04 13:54:11 +0200

    Added dashboard microfrontend

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,18 @@
-import React, { Suspense, lazy, useState } from 'react';
+import React, { Suspense, lazy, useState, useEffect } from 'react';
 import Header from './components/Header';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Router,
+  Redirect
+} from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName
 } from '@material-ui/core/styles';
 import Progress from './components/Progress';
+import { createBrowserHistory } from 'history';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
@@ -14,13 +21,20 @@ const generateClassName = createGenerateClassName({
 
 const MarketingApp = lazy(() => import('./components/MarketingApp'));
 const AuthApp = lazy(() => import('./components/AuthApp'));
+const DashboardApp = lazy(() => import('./components/DashboardApp'));
+
+const history = createBrowserHistory();
 
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) history.push('/dashboard');
+  }, [isSignedIn]);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header
             isSignedIn={isSignedIn}
@@ -31,11 +45,15 @@ export default () => {
               <Route path="/auth">
                 <AuthApp onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardApp />
+              </Route>
               <Route path="/" component={MarketingApp} />
             </Switch>
           </Suspense>
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
